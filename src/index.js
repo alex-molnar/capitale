144443cf2ad434e81896fa1408cd4f9b36b72950
Alex Molnar <alex.molnar@ing.com>
2026-08-04 15:25:05 +0200

    Centralized launch animation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { capitalize, unLe } from 'https://assets.kak.im/api/javascript/stringUti
 import { getRandomSelectionForToday, getItemForToday, getDirection, mathDistance } from 'https://assets.kak.im/api/javascript/mathHelpers.js'
 import { format } from 'https://assets.kak.im/api/javascript/stringUtils.js'
 import { loadGame } from 'https://assets.kak.im/api/javascript/gameHandler.js'
+import { launchConfetti } from 'https://assets.kak.im/api/javascript/animations.js'
 
 let five_mil = 5000000
 let mil = 1000000
@@ -176,35 +177,6 @@ function displayWinningGuessRow(triggerConfetti = false) {
     if (triggerConfetti) {
         launchConfetti()
     }
-}
-
-function launchConfetti() {
-    let container = document.getElementById('confetti-container')
-    if (!container) {
-        container = document.createElement('div')
-        container.id = 'confetti-container'
-        document.body.appendChild(container)
-    }
-    
-    const colors = ['#ff6b6b', '#4ecdc4', '#ffe66d', '#95e1d3', '#f38181', '#aa96da', '#fcbad3', '#a8d8ea', '#00a629', '#0b74de']
-    const shapes = ['square', 'circle', 'ribbon']
-    const confettiCount = 150
-    
-    for (let i = 0; i < confettiCount; i++) {
-        setTimeout(() => {
-            const confetti = document.createElement('div')
-            confetti.className = `confetti ${shapes[Math.floor(Math.random() * shapes.length)]}`
-            confetti.style.left = Math.random() * 100 + '%'
-            confetti.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
-            confetti.style.animationDuration = (Math.random() * 2 + 2) + 's'
-            confetti.style.animationDelay = Math.random() * 0.5 + 's'
-            container.appendChild(confetti)
-            
-            setTimeout(() => confetti.remove(), 4500)
-        }, i * 20)
-    }
-    
-    setTimeout(() => container.innerHTML = '', 5000)
 }
 
 document.title = `${gameTitle.capitalize()} v2`
